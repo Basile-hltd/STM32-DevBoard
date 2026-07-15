bed11c515ef74d456ea436eddc2932db1730db4d
Basile Hiltpold <basile.hiltpold@ludis-software.ch>
2026-07-15 16:39:32 +0200

    Blinky update

--- software/Blinky/src/main.c
+++ software/Blinky/src/main.c
@@ -5,20 +5,42 @@ int main(){
     STM32_DevBoard_HAL_Init();
 
     RGB_Off();
+    
+    Delay(2000);
 
-    rgb_led_color color = BLACK;
+    RGB_SetColor(RED);
 
-    Delay(500);
+    Delay(1000);
+
+    RGB_SetColor(GREEN);
+
+    Delay(1000);
+
+    RGB_SetColor(BLUE);
+
+    Delay(1000);
+
+    RGB_Off();
+
+    Delay(1000);
+
+    rgb_led_color color = WHITE;
 
     while(1){
-        color = (color + 1) % RGB_LED_COLOR_COUNT;
         RGB_SetColor(color);
 
         Delay(500);
 
         RGB_Off();
-        
+
         Delay(500);
+
+        color++;
+
+        if (color >= RGB_LED_COLOR_COUNT-1){
+            color = WHITE;
+        }
+
     }
 
 }
